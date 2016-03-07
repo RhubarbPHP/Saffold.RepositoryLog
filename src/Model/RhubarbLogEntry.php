782fa6ff25646468d341704d88d3434e6b357402
NiallSmyth <nsmyth@gcdtech.com>
2016-03-07 15:55:30 +0000

    Corrected classes for Column naming

--- src/Model/RhubarbLogEntry.php
+++ src/Model/RhubarbLogEntry.php
@@ -3,31 +3,31 @@
 namespace Rhubarb\Scaffolds\RepositoryLog\Model;
 
 use Rhubarb\Stem\Models\Model;
-use Rhubarb\Stem\Repositories\MySql\Schema\Columns\MySqlMediumText;
-use Rhubarb\Stem\Repositories\MySql\Schema\Index;
-use Rhubarb\Stem\Repositories\MySql\Schema\MySqlModelSchema;
+use Rhubarb\Stem\Repositories\MySql\Schema\Columns\MySqlMediumTextColumn;
 use Rhubarb\Stem\Schema\Columns\AutoIncrementColumn;
 use Rhubarb\Stem\Schema\Columns\DateTimeColumn;
 use Rhubarb\Stem\Schema\Columns\DecimalColumn;
 use Rhubarb\Stem\Schema\Columns\StringColumn;
+use Rhubarb\Stem\Schema\Index;
+use Rhubarb\Stem\Schema\ModelSchema;
 
 class RhubarbLogEntry extends Model
 {
-	public function createSchema()
-	{
-		$schema = new MySqlModelSchema("tblRhubarbLogEntry");
+    public function createSchema()
+    {
+        $schema = new ModelSchema("tblRhubarbLogEntry");
 
-		$schema->addColumn(
+        $schema->addColumn(
             new AutoIncrementColumn("RhubarbLogEntryID"),
             new StringColumn("LogSession", "30"),
             new DateTimeColumn("EntryDate"),
             new StringColumn("Category", 50),
-            new MySqlMediumText("Message"),
-            new MySqlMediumText("AdditionalData"),
+            new MySqlMediumTextColumn("Message"),
+            new MySqlMediumTextColumn("AdditionalData"),
             new StringColumn("IpAddress", 15),
             new DecimalColumn("ExecutionTime", 12, 4),
             new DecimalColumn("ExecutionGapTime", 12, 4)
-		);
+        );
 
         $schema->labelColumnName = "Message";
 
@@ -35,5 +35,5 @@ class RhubarbLogEntry extends Model
         $schema->addIndex(new Index("Category", Index::INDEX));
 
         return $schema;
-	}
+    }
 }
